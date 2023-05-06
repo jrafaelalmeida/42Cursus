98e17ac9fb0031dbd675f56c70a12348548ccdc2
Jose Rafael Pacheco De Almeida <jpacheco@c1r3s1.42porto.com>
2023-05-06 18:55:13 +0100

    Ready

--- Libft/ft_putstr_fd.c
+++ Libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: jpacheco <jpacheco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 21:14:29 by jpacheco          #+#    #+#             */
-/*   Updated: 2023/05/06 17:10:21 by jpacheco         ###   ########.fr       */
+/*   Updated: 2023/05/06 18:49:44 by jpacheco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,11 @@ void	ft_putstr_fd(char *s, int fd)
 	int	len;
 
 	len = 0;
-	if (!s)
+	if (!s || fd < 0)
 		return ;
 	while (s[len])
 	{
+		ft_putchar_fd(s[len], fd);
 		len++;
 	}
-	write(fd, s, len);
 }
-//check
